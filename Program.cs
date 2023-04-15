eae8604635985017da81e564ed10c20bf97092ea
Maxim_1623 <sabatag777@vivaldi.net>
2023-04-16 01:24:12 +0300

    Task 21 added

--- Program.cs
+++ Program.cs
@@ -9,19 +9,49 @@
 
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число");
-int num = Convert.ToInt32(Console.ReadLine());
-int n1=num/10000, n2=num/1000%10, n4=num/10%10, n5=num%10;
+// Console.WriteLine("Введите пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int n1=num/10000, n2=num/1000%10, n4=num/10%10, n5=num%10;
 
-if (10000<=num && num<100000)
-{
-                if (n1==n5 && n2==n4)
-            {
-                Console.WriteLine("Число является палиндромом");
-            }
-                else
-                Console.WriteLine("Число не является палиндромом");
+// if (10000<=num && num<100000)
+// {
+//                 if (n1==n5 && n2==n4)
+//             {
+//                 Console.WriteLine("Число является палиндромом");
+//             }
+//                 else
+//                 Console.WriteLine("Число не является палиндромом");
 
-}
-else
-Console.WriteLine("Введено неверное значение");
+// }
+// else
+// Console.WriteLine("Введено неверное значение");
+
+// ------------------------------------------------------------------------------------
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите X1");
+ int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y1");
+ int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z1");
+ int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите X2");
+ int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y2");
+ int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z2");
+ int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double d=Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+// Знак после запятой
+d = Math.Round(d,2);
+Console.WriteLine(d);
