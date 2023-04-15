2a26b481e19a0fda4adbd4aa526b3f8e7f440d43
Maxim_1623 <sabatag777@vivaldi.net>
2023-04-16 02:05:08 +0300

    Task 23 added

--- Program.cs
+++ Program.cs
@@ -37,21 +37,47 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите X1");
- int X1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y1");
- int Y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Z1");
- int Z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите X1");
+//  int X1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y1");
+//  int Y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Z1");
+//  int Z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите X2");
- int X2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y2");
- int Y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Z2");
- int Z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите X2");
+//  int X2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y2");
+//  int Y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Z2");
+//  int Z2 = Convert.ToInt32(Console.ReadLine());
 
-double d=Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+// double d=Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
 // Знак после запятой
-d = Math.Round(d,2);
-Console.WriteLine(d);
+// d = Math.Round(d,2);
+// Console.WriteLine(d);
+
+// ------------------------------------------------------------------------------------
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=num; i++)
+{     
+double d = (Math.Pow(i,3));
+
+            if (i==num)
+        {
+            Console.Write(d);
+        }
+            else
+                
+Console.Write(d+",");
+}
